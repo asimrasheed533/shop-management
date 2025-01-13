11c02665a89269ee9d4863d2d7e6b94c7eceb687
Asim Rasheed <asimrasheed442@gmail.com>
2025-01-13 09:49:05 +0500

    Update layout.tsx

--- app/employee/layout.tsx
+++ app/employee/layout.tsx
@@ -2,7 +2,14 @@ import React, { ReactNode } from "react";
 import "@/style/dashboard.scss";
 import DashboardLayout from "@/components/DashboardLayout";
 import EmployLink from "@/data/EmployLink";
-export default function layout({ children }: { children: ReactNode }) {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export default async function layout({ children }: { children: ReactNode }) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return redirect("/signIn");
+  }
   return (
     <>
       <DashboardLayout links={EmployLink}>{children}</DashboardLayout>
