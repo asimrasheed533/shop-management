28963e0db591c11225dde5c1b381e9b9210ebbef
Asim Rasheed <asimrasheed442@gmail.com>
2025-01-13 09:49:03 +0500

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,14 @@ import React, { ReactNode } from "react";
 import "@/style/dashboard.scss";
 import DashboardLayout from "@/components/DashboardLayout";
 import AdminLinks from "@/data/AdminLinks";
-export default function layout({ children }: { children: ReactNode }) {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export default async function layout({ children }: { children: ReactNode }) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return redirect("/signIn");
+  }
   return (
     <>
       <DashboardLayout links={AdminLinks}>{children}</DashboardLayout>
